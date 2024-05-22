6402ebf8b38c4e393c8b9d2d59dee7029e3bdc88
Finnedorn <daniele.giuntoli@outlook.com>
2024-05-22 14:15:47 +0200

    added basic scaffholding

---
+++ exercise.js
@@ -0,0 +1,8 @@
+// Definire un array di oggetti; ogni oggetto rappresenta un film o serie tv, che è caratterizzato da: title, year, genre, rating, type (movie o tv), seasons (solo per serie tv).
+// Creare una classe Movie che contenga le informazioni sopra indicate.
+// Creare una classe TvSerie che estenda la classe Movie e ne aggiunta la proprietà seasons.
+
+
+
+
+console.log('ciao mondo');
