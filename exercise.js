3b055a84e3fbf99abe3f73654d0cd0fdd62793df
Finnedorn <daniele.giuntoli@outlook.com>
2024-05-22 14:44:19 +0200

    added new ArrObj list / added new Movie and TvSerie classes / added map method for ArrObj list

--- exercise.js
+++ exercise.js
@@ -1,8 +1,84 @@
-// Definire un array di oggetti; ogni oggetto rappresenta un film o serie tv, che è caratterizzato da: title, year, genre, rating, type (movie o tv), seasons (solo per serie tv).
-// Creare una classe Movie che contenga le informazioni sopra indicate.
-// Creare una classe TvSerie che estenda la classe Movie e ne aggiunta la proprietà seasons.
+const netflixArchive = [
+    {
+      title: "Inception",
+      year: 2010,
+      genre: "Sci-Fi",
+      rating: 8.8,
+      type: "movie"
+    },
+    {
+      title: "Breaking Bad",
+      year: 2008,
+      genre: "Crime, Drama, Thriller",
+      rating: 9.5,
+      type: "tv",
+      seasons: 5
+    },
+    {
+      title: "The Dark Knight",
+      year: 2008,
+      genre: "Action, Crime, Drama",
+      rating: 9.0,
+      type: "movie"
+    },
+    {
+      title: "Stranger Things",
+      year: 2016,
+      genre: "Drama, Fantasy, Horror",
+      rating: 8.7,
+      type: "tv",
+      seasons: 4
+    },
+    {
+      title: "The Matrix",
+      year: 1999,
+      genre: "Action, Sci-Fi",
+      rating: 8.7,
+      type: "movie"
+    },
+    {
+      title: "Game of Thrones",
+      year: 2011,
+      genre: "Action, Adventure, Drama",
+      rating: 9.2,
+      type: "tv",
+      seasons: 8
+    }
+];
 
+class Movie {
+    constructor(title, year, genre, rating, type, seasons = null) {
+      this.title = title;
+      this.year = year;
+      this.genre = genre;
+      this.rating = rating;
+      this.type = type;
+      this.seasons = seasons;
+    }
 
+    toStringMovies() {
+        return `${this.title} è un film di genere ${this.genre}. E' stato rilasciato nel ${this.year} ed ha un voto di ${this.rating}.`;
+    }
+}
 
+class TvSerie extends Movie {
+    constructor(title, year, genre, rating, type, seasons) {
+      super(title, year, genre, rating, type);
+      this.seasons = seasons;
+    }
 
-console.log('ciao mondo');
+    toStringSeries() {
+        return `${this.title} è una serie tv di genere ${this.genre}. E' stata rilasciata nel ${this.year} ed in totale sono state prodotte ${this.seasons} stagioni. Ha un voto di ${this.rating}.`;
+    }
+    
+}
+
+netflixArchive.map((product) => {
+    if(product.type === "movie") {
+        product = new Movie(product.title, product.year, product.genre, product.rating, product.type);
+        console.log(product.toStringMovies());
+    } else {
+        product = new TvSerie(product.title, product.year, product.genre, product.rating, product.type, product.seasons);
+        console.log(product.toStringSeries());
+    }
+})
